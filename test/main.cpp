14e01ffc7bc916707bee241733fa3fa40f46b7dd
Chivelazur <chivelazur@gmail.com>
2020-07-03 08:06:05 +0300

    Uncomment main

--- test/main.cpp
+++ test/main.cpp
@@ -5,11 +5,11 @@
 void test() {
     qp::test::task_sort_order();
     qp::test::task_manager_wait();
-    //qp::test::sort_performance("sort_performance.csv");
-    //qp::test::performance_vs_thread(64, "performance_vs_thread.csv");
+    qp::test::sort_performance("sort_performance.csv");
+    qp::test::performance_vs_thread(64, "performance_vs_thread.csv");
     qp::test::performance_vs_set_size_fixed_total_runtime("performance_vs_set_size_fixed_total_runtime.csv");
-    //qp::test::performance_vs_set_size_fixed_task_duration("performance_vs_set_size_fixed_task_duraton.csv");
-    //qp::test::performance_vs_task_duration("performance_vs_task_duration.csv");
+    qp::test::performance_vs_set_size_fixed_task_duration("performance_vs_set_size_fixed_task_duraton.csv");
+    qp::test::performance_vs_task_duration("performance_vs_task_duration.csv");
 }
 
 int main() {
